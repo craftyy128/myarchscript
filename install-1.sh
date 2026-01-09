cc28fe2886a03bc9a946603d459dd0fe6bc2a331
craftyy128 <148287415+craftyy128@users.noreply.github.com>
2026-01-09 16:10:28 -0500

    Refactor install script for package installation
    Removed initial installation instructions and added package installation commands, including setup for icons, gamemode, yay, kitty themes, and zsh.

--- install-1.sh
+++ install-1.sh
@@ -1,39 +1,33 @@
 #!/bin/bash/
-
-echo "hello dis my first actual project so yea hi me"
-
-sleep 2
-
-echo "OK im assuming youve done the partitioning so we gunna follow the arch install guide k? so lets get somthing straigh /dev/nvme0n1p1 is efi /dev/nvme0n1p2 is root /dev/nvme0n1p3 is swap K?"
-
-sleep 4
-
-#Mount commands
-
-timedatectl
-
-mount /dev/nvme0n1p2 /mnt
-
-mount --mkdir /dev/nvme0n1p1 /mnt/boot
-
-swapon /dev/nvme0n1p3
-
-echo "yay!!! weve mounted the system!!!! now lets install it!"
-
-sleep 2
-
-#Install system
-
-pacstrap -K /mnt base linux linux-firmware sof-firmware intel-ucode firefox nano
-
-echo "alright now lets generate the fstab file!"
-
-#genfstab (why u looking here? well anyways....)
-
-genfstab -U /mnt >> /mnt/etc/fstab
-
-echo "yessir weve done everything we ment to do!!! now just do arch-chroot /mnt to continue
-
-
-
+# installing some packages like gnome and other stuff
+sudo pacman -S wget discord prismlauncher zsh kitty gnome firefox flatpak meson systemd git dbus libinih gcc pkgconf power-profiles-daemon cpupower starship
+# doing gits and stuff
+# papirus icons
+wget -qO- https://git.io/papirus-icon-theme-install | sh
+# win10os cursor
+mkdir /usr/share/icons/ Win10OS-cursors
+git clone https://github.com/yeyushengfan258/Win10OS-cursors.git
+cd Win10OS-cursors
+./install.sh
+cd
+# gamemode 
+git clone https://github.com/FeralInteractive/gamemode.git
+cd gamemode
+git checkout 1.8.2 
+./bootstrap.sh
+sudo groupadd gamemode
+sudo usermod -aG gamemode craftyy
+gamemoded -t
+# getting yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+# kitty stuff
+git clone --depth 1 https://github.com/dexpota/kitty-themes.git ~/.config/kitty/kitty-themes
+cd ~/.config/kitty
+ln -s ./kitty-themes/themes/AtelierSulphurpool.conf ~/.config/kitty/theme.conf
+echo "include ./theme.conf" >> ~/.config/kitty/kitty.conf
+# making zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+# nerd font
 
